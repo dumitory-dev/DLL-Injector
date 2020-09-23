cdd6138f13bc0c2d8991c1f3b8cc5dfdd16a01f4
Dmitry <45358107+DumitoryDev@users.noreply.github.com>
2020-09-23 17:23:13 +0300

    Fix: refactoring

--- Injector/Source/main.c
+++ Injector/Source/main.c
@@ -1,13 +1,10 @@
 ﻿#include <Windows.h>
 
-BOOL APIENTRY DllMain(
-	HMODULE hModule,
-	const DWORD ulReasonForCall,
-	LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, const DWORD ulReasonForCall,
+                      LPVOID lpReserved)
 {
-
-	UNREFERENCED_PARAMETER(hModule);
-	UNREFERENCED_PARAMETER(lpReserved);
-	UNREFERENCED_PARAMETER(ulReasonForCall);
-	return TRUE;
+    UNREFERENCED_PARAMETER(hModule);
+    UNREFERENCED_PARAMETER(lpReserved);
+    UNREFERENCED_PARAMETER(ulReasonForCall);
+    return TRUE;
 }
