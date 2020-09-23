85b4b705497a8f26d1b0fab139f4e5838c69f8cd
Dmitry <45358107+DumitoryDev@users.noreply.github.com>
2020-09-23 14:49:55 +0300

    Update Injector.h

--- Injector/Header/Injector.h
+++ Injector/Header/Injector.h
@@ -8,7 +8,7 @@ EXTERN_C_START
 LPTHREAD_START_ROUTINE getCorrectLoadLibrary(HANDLE hProcess);
 
 BOOL injectX86X64(const char *dllPath, DWORD pid);
-BOOL injectX86Only(const char *dllPath, DWORD pid);
+BOOL injectX86(const char *dllPath, DWORD pid);
 BOOL inject(const char *dllPath, DWORD pid);
 
 SIZE_T getFunctionAddress32(const char *moduleName, const char *functionName,
